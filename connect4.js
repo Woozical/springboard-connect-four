3f2d4719b3359a9caef4b980c44a6e029eb779f8
Mark <mark@example.dev>
2021-07-09 16:58:52 -0700

    tie checking implementation

--- connect4.js
+++ connect4.js
@@ -15,14 +15,14 @@ const STATE = {awaitStart : 0, inProg : 1, over : 2};
 let gameState = STATE.awaitStart;
 
 let currPlayer = 1; // active player: 1 or 2
-const board = []; // array of rows, each row is array of cells  (board[y][x])
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  // set "board" to empty HEIGHT x WIDTH matrix array
   for (let i = 0; i < HEIGHT; i++){
     const newRow = [];
     newRow.length = WIDTH;
@@ -84,7 +84,7 @@ function findSpotForCol(x) {
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
+  // make a div and insert into correct table cell
   const cell = document.getElementById(`${y}-${x}`);
   const piece = document.createElement('div');
   piece.classList.add('piece');
@@ -130,15 +130,14 @@ function handleClick(evt) {
 
   // get x from ID of clicked cell
   const x = +evt.target.id;
-  console.log('X:',x); //DEBUG
+  //console.log('X:',x); //DEBUG
   // get next spot in column (if none, ignore click)
   const y = findSpotForCol(x);
   if (y === null) {
     return;
   }
-  console.log('Y:',y);
+  //console.log('Y:',y);
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
   placeInTable(y, x);
   board[y][x] = currPlayer;
 
@@ -148,14 +147,54 @@ function handleClick(evt) {
   }
 
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
+  if (checkForTie()) {
+    return endGame('A tie! Game over.');
+  }
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
   currPlayer = currPlayer === 1 ? 2 : 1;
   document.getElementById('p1Token').classList.toggle('selected');
   document.getElementById('p2Token').classList.toggle('selected');
 }
+
+// checkForTie: check to see if board is full with no winner
+
+function checkForTie(){
+
+  return board.every(
+    (row) => {
+      return row.every(
+        (cell) => {
+          return (cell !== undefined);
+        }
+      )
+    }
+  )
+}
+
+// DEBUG
+function debugMakeTie(){
+  board = [
+    [2, 2, undefined, 2, 2, 1, 2],
+    [1, 1, 2, 1, 1, 1, 2],
+    [2, 2, 1, 2, 2, 1, 2],
+    [1, 1, 2, 1, 1, 2, 1],
+    [2, 2, 1, 2, 2, 1, 2],
+    [1, 1, 2, 1, 1, 2, 1]
+  ];
+
+  for (let y = 0; y < HEIGHT; y++){
+    for (let x = 0; x < WIDTH; x++){
+      currPlayer = board[y][x];
+      if (y === 0 && x === 2){
+        continue;
+      }
+      placeInTable(y, x);
+    }
+  }
+
+}
+
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
 
@@ -208,7 +247,7 @@ function controlHandler(e){
       case STATE.over:
         clearBoard();
         clearHtmlBoard();
-        e.target.removeAttribute('disabled');
+        e.target.setAttribute('disabled', '');
         document.getElementById('p1Token').classList.add('selected');
         document.getElementById('p2Token').classList.remove('selected');
         gameState = STATE.inProg;
