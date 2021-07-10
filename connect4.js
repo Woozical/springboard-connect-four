9668db34796ec547c8468e6338222aed792db827
Mark <mark@example.dev>
2021-07-09 20:13:08 -0700

    some basic styling

--- connect4.js
+++ connect4.js
@@ -18,7 +18,7 @@ let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 let player1Color = 'red';
-let player2Color = 'blue';
+let player2Color = 'yellow';
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
